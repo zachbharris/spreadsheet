bab5245499043d6d62609ddf6a43cc8047a8ec26
Zach Harris <zachbharris94@gmail.com>
2019-06-05 21:18:40 -0500

    modify, add, remove columns and rows

--- client/src/routes/Sheet/index.jsx
+++ client/src/routes/Sheet/index.jsx
@@ -1,7 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initialData = [
+  ['', '0', '1'],
+  ['a', '', ''],
+  ['b', '', ''],
+  ['c', '', ''],
+  ['d', '', '']
+]
 
 const Sheet = () => {
-  return <p>sheet</p>
+  const [data, set] = useState(initialData)
+  const handleChange = (event, row, col) => {
+    const { value } = event.target
+
+    // dr = data row
+    // dc = data column
+    const arr = data.map((dr, i) => {
+      return i === row
+        ? dr.map((dc, ind) => (ind === col ? (dc = value) : dc))
+        : dr
+    })
+
+    return set(arr)
+  }
+
+  const addColumn = () => {
+    const arr = data.map((dr, i) => {
+      dr.push('')
+      return dr
+    })
+    return set(arr)
+  }
+
+  const removeColumn = () => {
+    const arr = data.map(dr => {
+      dr.pop()
+      return dr
+    })
+    return set(arr)
+  }
+  return (
+    <>
+      <button onClick={addColumn}>Add</button>
+      <button onClick={removeColumn}>Remove</button>
+      <table>
+        <thead>
+          <tr>
+            {data[0].map((dc, col) => {
+              return (
+                <th key={col}>
+                  <input value={dc} onChange={e => handleChange(e, 0, col)} />
+                </th>
+              )
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((dr, row) => {
+            return (
+              <tr key={row}>
+                {dr.map((dc, col) => {
+                  if (row === 0) return null
+                  return (
+                    <td key={col}>
+                      <input
+                        value={dc}
+                        onChange={e => handleChange(e, row, col)}
+                      />
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
+  )
+  // return data.map((d, row) => {
+  //   return (
+  //     <div key={row}>
+  //       {d.map((item, col) => {
+  //         return (
+  //           <input
+  //             key={col}
+  //             value={item}
+  //             onChange={e => handleChange(e, row, col)}
+  //           />
+  //         )
+  //       })}
+  //     </div>
+  //   )
+  // })
 }
 
 export default Sheet
