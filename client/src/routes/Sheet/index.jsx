352b7f4837abeea4aeb475d4e974ab2823997a2f
Zach Harris <zachbharris94@gmail.com>
2019-06-05 21:51:39 -0500

    working mockup

--- client/src/routes/Sheet/index.jsx
+++ client/src/routes/Sheet/index.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 
 const initialData = [
-  ['', '0', '1'],
-  ['a', '', ''],
-  ['b', '', ''],
-  ['c', '', ''],
-  ['d', '', '']
+  [
+    { type: 'text', value: '' },
+    { type: 'text', value: '' },
+    { type: 'number', value: '' }
+  ],
+  [{ value: '' }, { value: '' }, { value: '' }],
+  [{ value: '' }, { value: '' }, { value: '' }],
+  [{ value: '' }, { value: '' }, { value: '' }],
+  [{ value: '' }, { value: '' }, { value: '' }]
 ]
 
 const Sheet = () => {
@@ -17,39 +21,77 @@ const Sheet = () => {
     // dc = data column
     const arr = data.map((dr, i) => {
       return i === row
-        ? dr.map((dc, ind) => (ind === col ? (dc = value) : dc))
+        ? dr.map((dc, ind) => (ind === col ? { ...dc, value } : dc))
         : dr
+    })
+    return set(arr)
+  }
+
+  const handleTypeChange = (event, col) => {
+    const { value } = event.target
+
+    // dr = data row
+    // dc = data column
+    const arr = data.map((dr, row) => {
+      if (row !== 0) return dr
+      return dr.map((dc, i) => {
+        return i === col ? { ...dc, type: value } : dc
+      })
     })
 
     return set(arr)
   }
 
   const addColumn = () => {
-    const arr = data.map((dr, i) => {
-      dr.push('')
+    // dr = data row
+    const arr = data.map(dr => {
+      dr.push({ type: '', value: '' })
       return dr
     })
     return set(arr)
   }
 
   const removeColumn = () => {
+    // dr = data row
     const arr = data.map(dr => {
       dr.pop()
       return dr
     })
     return set(arr)
   }
+
+  const addRow = () => {
+    let mutation = []
+    for (let i = 0; i < data[0].length; i++) {
+      mutation.push({ value: '' })
+    }
+    const arr = [...data, mutation]
+    return set(arr)
+  }
+
   return (
     <>
-      <button onClick={addColumn}>Add</button>
-      <button onClick={removeColumn}>Remove</button>
+      <button onClick={addColumn}>Add Column</button>
+      <button onClick={removeColumn}>Remove Column</button>
+      <button onClick={addRow}>Add Row</button>
       <table>
         <thead>
           <tr>
             {data[0].map((dc, col) => {
               return (
                 <th key={col}>
-                  <input value={dc} onChange={e => handleChange(e, 0, col)} />
+                  <input
+                    type={dc.type}
+                    value={dc.value}
+                    onChange={e => handleChange(e, 0, col)}
+                  />
+                  <select
+                    onChange={e => handleTypeChange(e, col)}
+                    value={dc.type}
+                  >
+                    <option value="text">text</option>
+                    <option value="number">number</option>
+                  </select>
                 </th>
               )
             })}
@@ -64,7 +106,8 @@ const Sheet = () => {
                   return (
                     <td key={col}>
                       <input
-                        value={dc}
+                        type={data[0][col].type}
+                        value={dc.value}
                         onChange={e => handleChange(e, row, col)}
                       />
                     </td>
